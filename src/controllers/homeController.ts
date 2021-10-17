31aeef4ceab236bf28eb85037c863c175c2472f8
Amós Silas <silastj@hotmail.com>
2021-10-17 12:38:06 -0300

    add more code model/controllers e views

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from 'express'
 
+import {NewProduct} from '../models/Product'
+
 export const home = (req: Request, res: Response) =>{
 
     let idade : number = 51
@@ -9,6 +11,11 @@ export const home = (req: Request, res: Response) =>{
         showIdade = true
     }
 
+    // pegando todos os produtos
+    let productAll = NewProduct.getAll();
+    //filtrando por price igual ou acima de 12 reais
+    let priceChosen = 3000
+    let productFilter = NewProduct.getBigFilters(priceChosen)
 
     let name = "Amós"
     res.render('pages/home',{
@@ -16,11 +23,8 @@ export const home = (req: Request, res: Response) =>{
         name,
         idade,
         showIdade,
-        products:[
-            {title:'Amós', idade:37},
-            {title:'Samara', idade: 25},
-            {title:'Calebe', idade: 1}
-        ],
+        products: productAll,
+        products12: productFilter,
         carros:[]
     })
 }
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -7,12 +7,26 @@ type Product = { // tipando os dados
 const data: Product[] = [// recebendo as informações como fosse o banco de dados
     { title: 'Celular', price: 3097 },
     { title: 'Camera', price: 2500 },
-    { title: 'TV', price: 10900 }
+    { title: 'TV', price: 10900 },
+    { title: 'VideoCassete', price: 900 }
 ]
 
 // Criei um objeto e dentro um metodo getAll tipando o metodo que ira receber um array
 export const NewProduct = {
     getAll: (): Product[] => {
         return data
+    },
+// Novo metodo para filtrar por parametro passando price
+    getBigFilters: (price:number): Product[] => {
+        return data.filter(item => {
+            if(item.price >= price){
+                return true
+            }
+            else{
+                return false
+            }
+        })
     }
+
+
 }
